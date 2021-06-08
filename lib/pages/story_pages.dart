f5c947b0470fc070599691578394136bfb49645c
guakoko <gua.koko1@gmail.com>
2021-06-08 08:44:53 +0700

    ini backup dari sleep m1 error

--- lib/pages/story_pages.dart
+++ lib/pages/story_pages.dart
@@ -5,6 +5,56 @@ class StoryPages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: themeColor,
+      body: SafeArea(
+        bottom: false,
+        child: ListView(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 290,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(25),
+                ),
+                image: DecorationImage(
+                    image: AssetImage(
+                      'assets/moon1.png',
+                    ),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Sleep Time',
+                    style: titleTextStyle.copyWith(
+                      fontSize: 30,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Ease the mind into a restful night’s sleep with\nthese deep, amblent tones.',
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
